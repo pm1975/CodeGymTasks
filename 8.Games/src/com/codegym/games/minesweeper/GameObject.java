5805427945fc75148a938ef28050a8fdb4023282
pm1975 <pm1975@windowslive.com>
2020-06-19 19:21:47 +0200

    Minesweeper (Part 3/16) passed

---
+++ 8.Games/src/com/codegym/games/minesweeper/GameObject.java
@@ -0,0 +1,14 @@
+package com.codegym.games.minesweeper;
+
+/**
+ * Created by Piotr on 2020-06-19
+ */
+public class GameObject {
+    public int x;
+    public int y;
+
+    public GameObject(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+}
