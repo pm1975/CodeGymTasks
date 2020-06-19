d792a524e9cf8b99c3eca9cebdbf85acdbe3533c
pm1975 <pm1975@windowslive.com>
2020-06-19 19:18:07 +0200

    Minesweeper (Part 2/16) passed

--- 8.Games/src/com/codegym/games/minesweeper/MinesweeperGame.java
+++ 8.Games/src/com/codegym/games/minesweeper/MinesweeperGame.java
@@ -6,4 +6,16 @@ import com.codegym.engine.cell.*;
  * Created by Piotr on 2020-06-19
  */
 public class MinesweeperGame extends Game {
+    private static final int SIDE = 9;
+    
+    @Override
+    public void initialize() {
+        super.initialize();
+        setScreenSize(SIDE, SIDE);
+    }
+
+    @Override
+    public void setScreenSize(int width, int height) {
+        super.setScreenSize(width, height);
+    }
 }
