0e3a7e24f19382302165240a9d3ea2891355cec1
pm1975 <pm1975@windowslive.com>
2020-10-15 21:28:24 +0200

    Snake (Part 3/20)

---
+++ 8.Games/src/com/codegym/games/snake/GameObject.java
@@ -0,0 +1,14 @@
+package com.codegym.games.snake;
+
+/**
+ * Created by Piotr on 2020-10-15
+ */
+public class GameObject {
+    public int x;
+    public int y;
+
+    public GameObject(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+}
