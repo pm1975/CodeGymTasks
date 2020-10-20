377ecdef484dd3a1cd0ed763fadf993836d52a84
pm1975 <pm1975@windowslive.com>
2020-10-20 19:43:57 +0200

    Moon Lander (Part 3/23)

---
+++ 8.Games/src/com/codegym/games/moonlander/GameObject.java
@@ -0,0 +1,14 @@
+package com.codegym.games.moonlander;
+
+/**
+ * Created by Piotr on 2020-10-20
+ */
+public class GameObject {
+    public double x;
+    public double y;
+
+    public GameObject(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
+}
