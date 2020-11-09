8713fc07d4a93abaf7e9bdb4043ebb04c8ce96e3
pm1975 <pm1975@windowslive.com>
2020-11-09 22:23:23 +0100

    task1904

--- 2.JavaCore/src/pl/codegym/task/task18/task1824/Solution.java
+++ 2.JavaCore/src/pl/codegym/task/task18/task1824/Solution.java
@@ -7,22 +7,16 @@ Pliki i wyjątki
 import java.io.*;
 
 public class Solution {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-        String fileName;
-        try {
-            while (true) {
-                fileName = reader.readLine();
-                if (new File(fileName).exists()) {
-                    FileInputStream inputStream = new FileInputStream(fileName);
-                    inputStream.close();
-                } else {
-                    System.out.println(fileName);
-                    throw new FileNotFoundException();
-                }
+        while (true) {
+            String fileName = reader.readLine();
+            try (FileInputStream inputStream = new FileInputStream(fileName)) {
+
+            } catch (FileNotFoundException e) {
+                System.out.printf(fileName);
+                break;
             }
-        } catch (IOException e) {
-            e.printStackTrace();
         }
     }
 }
---
+++ 2.JavaCore/src/pl/codegym/task/task19/task1904/Solution.java
@@ -0,0 +1,19 @@
+package pl.codegym.task.task19.task1904;
+
+import java.io.IOException;
+import java.util.Scanner;
+
+/* 
+Jeszcze jeden adapter
+*/
+
+public class Solution {
+
+    public static void main(String[] args) {
+
+    }
+
+    public static class PersonScannerAdapter {
+
+    }
+}
