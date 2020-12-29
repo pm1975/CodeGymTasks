f22e84d7a0cdfe5ea4c95bb416d9d0f9b73bb9d7
pm1975 <pm1975@windowslive.com>
2020-12-29 23:48:31 +0100

    task2304-8

---
+++ 3.JavaMultithreading/src/com/codegym/task/task23/task2309/Solution.java
@@ -0,0 +1,26 @@
+package com.codegym.task.task23.task2309;
+
+import com.codegym.task.task23.task2309.vo.*;
+
+import java.util.List;
+
+/* 
+Sometimes anonymity is so nice!
+
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        print(solution.getUsers());
+        print(solution.getLocations());
+    }
+
+    public static void print(List list) {
+        String format = "Id=%d, name='%s', description=%s";
+        for (Object obj : list) {
+            NamedItem item = (NamedItem) obj;
+            System.out.println(String.format(format, item.getId(), item.getName(), item.getDescription()));
+        }
+    }
+}
