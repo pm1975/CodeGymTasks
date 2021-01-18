8bc9c4fb1fc8d4b236975de4af94928e616a4063
pm1975 <pm1975@windowslive.com>
2021-01-18 23:47:37 +0100

    Racer (Part 8/28)

--- 8.Games/src/com/codegym/games/racer/GameObject.java
+++ 8.Games/src/com/codegym/games/racer/GameObject.java
@@ -17,10 +17,10 @@ public class GameObject {
         this.height = matrix.length;
     }
 
-    public void draw(Game game) {
+    public void draw(Game game){
         for (int i = 0; i < matrix.length; i++) {
             for (int j = 0; j < matrix[0].length; j++) {
-                game.setCellColor(x + j, y + i, Color.values()[matrix[i][j]]);
+                game.setCellColor(x+j, y + i, Color.values()[matrix[i][j]]);
             }
         }
     }
--- 8.Games/src/com/codegym/games/racer/PlayerCar.java
+++ 8.Games/src/com/codegym/games/racer/PlayerCar.java
@@ -3,8 +3,9 @@ package com.codegym.games.racer;
 public class PlayerCar extends GameObject {
 
     private static int playerCarHeight = ShapeMatrix.PLAYER.length;
+    public int speed = 1;
 
     public PlayerCar() {
-        super(RacerGame.WIDTH / 2 + 2, RacerGame.HEIGHT - playerCarHeight - 1, ShapeMatrix.PLAYER);
+        super(RacerGame.WIDTH/2 + 2, RacerGame.HEIGHT - playerCarHeight - 1, ShapeMatrix.PLAYER);
     }
 }
