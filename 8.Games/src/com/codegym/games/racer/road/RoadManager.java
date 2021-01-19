1eea8e62bbd22bda5d8d1d18b2c394afae5d66ad
pm1975 <pm1975@windowslive.com>
2021-01-19 20:35:04 +0100

    Racer (Part 11/28) after

--- 8.Games/src/com/codegym/games/racer/road/RoadManager.java
+++ 8.Games/src/com/codegym/games/racer/road/RoadManager.java
@@ -5,4 +5,12 @@ import com.codegym.games.racer.RacerGame;
 public class RoadManager {
     public static final int LEFT_BORDER = RacerGame.ROADSIDE_WIDTH;
     public static final int RIGHT_BORDER = RacerGame.WIDTH - RacerGame.ROADSIDE_WIDTH;
+
+    private RoadObject createRoadObject(RoadObjectType type, int x, int y) {
+        if (type==RoadObjectType.SPIKE) {
+            return new Spike(x, y);
+        } else {
+            return null;
+        }
+    }
 }
--- 8.Games/src/com/codegym/games/racer/road/RoadObject.java
+++ 8.Games/src/com/codegym/games/racer/road/RoadObject.java
@@ -17,20 +17,10 @@ public class RoadObject extends GameObject {
         this.height = matrix.length;
     }
 
-    /**
-     * The method responsible for moving an obstacle. An obstacle can have its own speed and a secondary speed
-     * that depends on the player's speed.
-     */
     public void move(int boost) {
         this.y += boost;
     }
 
-    /**
-     * Checks whether the images of the current object and the passed object are
-     * closer than the value of the distance parameter.
-     * For example, if the distance 12 is passed and the 2 objects are separated by less than
-     * 12 cells on the playing field, the method returns true. Otherwise, it returns false.
-     */
     public boolean isCollisionWithDistance(RoadObject roadObject, int distance) {
         if ((x - distance > roadObject.x + roadObject.width) || (x + width + distance < roadObject.x)) {
             return false;
@@ -43,9 +33,6 @@ public class RoadObject extends GameObject {
         return true;
     }
 
-    /**
-     * Returns the object's image matrix based on its type.
-     */
     private static int[][] getMatrix(RoadObjectType type) {
         switch (type) {
             case CAR:
@@ -63,9 +50,6 @@ public class RoadObject extends GameObject {
         }
     }
 
-    /**
-     * Returns the object's height.
-     */
     public static int getHeight(RoadObjectType type) {
         return getMatrix(type).length;
     }
