f370c11ada37ca72cfdd72a0d9054c6d4bace109
pm1975 <pm1975@windowslive.com>
2021-01-21 21:53:04 +0100

    Racer (Part 24/28) after

--- 8.Games/src/com/codegym/games/racer/RacerGame.java
+++ 8.Games/src/com/codegym/games/racer/RacerGame.java
@@ -32,6 +32,9 @@ public class RacerGame extends Game {
             return;
         }
         roadManager.generateNewRoadObjects(this);
+        if (roadManager.getPassedCarsCount() >= RACE_GOAL_CARS_COUNT) {
+            finishLine.show();
+        }
         moveAll();
         drawScene();
     }
