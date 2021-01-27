dac3faa548192aaf7e32f5df1daa4269b7ce30a5
pm1975 <pm1975@windowslive.com>
2021-01-27 22:13:48 +0100

    Space Invaders (Part 18/34) before

--- 8.Games/src/com/codegym/games/spaceinvaders/gameobjects/PlayerShip.java
+++ 8.Games/src/com/codegym/games/spaceinvaders/gameobjects/PlayerShip.java
@@ -3,10 +3,28 @@ package com.codegym.games.spaceinvaders.gameobjects;
 import com.codegym.games.spaceinvaders.ShapeMatrix;
 import com.codegym.games.spaceinvaders.SpaceInvadersGame;
 
+import java.util.List;
+
 public class PlayerShip extends Ship {
 
     public PlayerShip() {
         super(SpaceInvadersGame.WIDTH / 2., SpaceInvadersGame.HEIGHT - ShapeMatrix.PLAYER.length - 1);
         setStaticView(ShapeMatrix.PLAYER);
+    }
+
+    public void checkHit(List<Bullet> bullets) {
+        if (bullets.isEmpty()) {
+            return;
+        }
+
+        if (isAlive) {
+            for (Bullet bullet : bullets) {
+                if (isAlive && bullet.isAlive && isCollision(bullet)) {
+                    kill();
+                    bullet.kill();
+                    return;
+                }
+            }
+        }
     }
 }
