af40dbf5351da5b59fdc0d19ae776529ae481533
pm1975 <pm1975@windowslive.com>
2021-01-29 18:46:31 +0100

    EASY task2514 (task2513 done)

--- 3.JavaMultithreading/src/com/codegym/task/task25/task2512/Solution.java
+++ 3.JavaMultithreading/src/com/codegym/task/task25/task2512/Solution.java
@@ -5,10 +5,23 @@ Charting our own course
 
 */
 
+import java.util.LinkedList;
+
 public class Solution implements Thread.UncaughtExceptionHandler {
 
     @Override
     public void uncaughtException(Thread t, Throwable e) {
+        t.interrupt();
+
+        LinkedList<String> res = new LinkedList<>();
+        while (e != null) {
+            res.add(0, e.toString());
+            e = e.getCause();
+        }
+
+        for (String s : res) {
+            System.out.println(s);
+        }
     }
 
     public static void main(String[] args) {
