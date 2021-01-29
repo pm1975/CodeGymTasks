d6229ff5b71530bfb75bbad8fb673c4ad8fbec76
pm1975 <pm1975@windowslive.com>
2021-01-29 18:23:28 +0100

    MEDIUM task2512 (task2511 done)

---
+++ 3.JavaMultithreading/src/com/codegym/task/task25/task2512/Solution.java
@@ -0,0 +1,16 @@
+package com.codegym.task.task25.task2512;
+
+/* 
+Charting our own course
+
+*/
+
+public class Solution implements Thread.UncaughtExceptionHandler {
+
+    @Override
+    public void uncaughtException(Thread t, Throwable e) {
+    }
+
+    public static void main(String[] args) {
+    }
+}
