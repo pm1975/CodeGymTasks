fc420bbd6a207826f71d9e4fca969821ec117657
pm1975 <pm1975@windowslive.com>
2021-02-04 18:37:28 +0100

    MEDIUM Refactoring (part 10). Done with little help.

---
+++ 3.JavaMultithreading/src/com/codegym/task/task29/task2909/car/Sedan.java
@@ -0,0 +1,9 @@
+package com.codegym.task.task29.task2909.car;
+
+public class Sedan extends Car {
+
+    public Sedan(int numberOfPassengers) {
+        super(Car.SEDAN, numberOfPassengers);
+    }
+
+}
