3430a5946e1290832fe4b0f67a492221e567d194
pm1975 <pm1975@windowslive.com>
2021-02-11 21:24:44 +0100

    java.util.concurrent.locks.Lock task. Start.

---
+++ 3.JavaMultithreading/src/com/codegym/task/task26/task2612/Solution.java
@@ -0,0 +1,23 @@
+package com.codegym.task.task26.task2612;
+
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+/* 
+The whole world is a comedy
+
+*/
+
+public class Solution {
+    protected Lock lock = new ReentrantLock();
+
+    public void someMethod() {
+        // Implement the logic here. Use the lock field
+    }
+
+    public void actionIfLockIsFree() {
+    }
+
+    public void actionIfLockIsBusy() {
+    }
+}
