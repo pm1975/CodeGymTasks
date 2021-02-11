d4228566532d7f5c56a6ff68213447ed1bcfceec
pm1975 <pm1975@windowslive.com>
2021-02-11 23:27:49 +0100

    HARD. Chat (part 5). Start.

--- 3.JavaMultithreading/src/com/codegym/task/task30/task3008/ConsoleHelper.java
+++ 3.JavaMultithreading/src/com/codegym/task/task30/task3008/ConsoleHelper.java
@@ -1,10 +1,11 @@
 package com.codegym.task.task30.task3008;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class ConsoleHelper {
-    private static BufferedReader bis = new BufferedReader(new InputStreamReader(System.in));;
+    private static BufferedReader bis = new BufferedReader(new InputStreamReader(System.in));
 
     public static void writeMessage(String message) {
         System.out.println(message);
@@ -14,10 +15,9 @@ public class ConsoleHelper {
         while (true) {
             try {
                 String buf = bis.readLine();
-                if (buf != null) {
+                if (buf != null)
                     return buf;
-                }
-            } catch (Exception e) {
+            } catch (IOException e) {
                 writeMessage("An error occurred while trying to enter text. Try again.");
             }
         }
