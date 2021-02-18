75ebe1b78ee419b8ecb5561905bdf3430f1d7641
pm1975 <pm1975@windowslive.com>
2021-02-18 19:27:05 +0100

    HARD. Chat (part 6). Done with help.

--- 3.JavaMultithreading/src/com/codegym/task/task30/task3008/Connection.java
+++ 3.JavaMultithreading/src/com/codegym/task/task30/task3008/Connection.java
@@ -14,8 +14,8 @@ public class Connection implements Closeable {
 
     public Connection(Socket socket) throws IOException {
         this.socket = socket;
-        out = new ObjectOutputStream(socket.getOutputStream());
-        in = new ObjectInputStream(socket.getInputStream());
+        this.out = new ObjectOutputStream(socket.getOutputStream());
+        this.in = new ObjectInputStream(socket.getInputStream());
     }
 
     public void send(Message message) throws IOException {
@@ -34,6 +34,7 @@ public class Connection implements Closeable {
         return socket.getRemoteSocketAddress();
     }
 
+    @Override
     public void close() throws IOException {
         in.close();
         out.close();
