75074465755e56f82693a1d74d607f5058a2daee
pm1975 <pm1975@windowslive.com>
2021-02-26 18:54:34 +0100

    2710 MEDIUM. Arrange wait and notify methods. Start.

---
+++ 3.JavaMultithreading/src/com/codegym/task/task27/task2710/MailServer.java
@@ -0,0 +1,18 @@
+package com.codegym.task.task27.task2710;
+
+public class MailServer implements Runnable {
+    private Mail mail;
+
+    public MailServer(Mail mail) {
+        this.mail = mail;
+    }
+
+    @Override
+    public void run() {
+        long startTime = System.currentTimeMillis();
+        // do something here
+        String name = Thread.currentThread().getName();
+        long endTime = System.currentTimeMillis();
+        System.out.format("%s MailServer received: [%s] in %d ms after start", name, mail.getText(), (endTime - startTime));
+    }
+}
---
+++ 3.JavaMultithreading/src/com/codegym/task/task27/task2710/Person.java
@@ -0,0 +1,22 @@
+package com.codegym.task.task27.task2710;
+
+public class Person implements Runnable {
+    private final Mail mail;
+
+    public Person(Mail mail) {
+        this.mail = mail;
+    }
+
+    @Override
+    public void run() {
+        String name = Thread.currentThread().getName();
+        try {
+            Thread.sleep(1000);
+            // do something here
+            mail.setText("Person [" + name + "] wrote an email 'AAA'");
+            // do something here
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
