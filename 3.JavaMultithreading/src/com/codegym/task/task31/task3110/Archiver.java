bcb72cbe947e989fce7bfdef3ff7aea80aabd9e5
pm1975 <pm1975@windowslive.com>
2021-02-26 22:12:39 +0100

    Archiver (part 3). Done.

--- 3.JavaMultithreading/src/com/codegym/task/task31/task3110/Archiver.java
+++ 3.JavaMultithreading/src/com/codegym/task/task31/task3110/Archiver.java
@@ -1,10 +1,12 @@
 package com.codegym.task.task31.task3110;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.nio.file.Paths;
 
 public class Archiver {
+
     public static void main(String[] args) throws Exception {
         BufferedReader bis = new BufferedReader(new InputStreamReader(System.in));
 
