124ec43b3bafad6082b67e0b476e431e098d30c6
pm1975 <pm1975@windowslive.com>
2021-02-27 08:53:40 +0100

    Archiver (part 12). Done.

--- 3.JavaMultithreading/src/com/codegym/task/task31/task3110/command/ZipCommand.java
+++ 3.JavaMultithreading/src/com/codegym/task/task31/task3110/command/ZipCommand.java
@@ -8,7 +8,7 @@ import java.nio.file.Paths;
 
 public abstract class ZipCommand implements Command {
 
-    public ZipFileManager getZipFileManager() throws Exception {
+    public ZipFileManager getZipFileManager() throws Exception{
         ConsoleHelper.writeMessage("Enter the full path to the archive file:");
         Path zipPath = Paths.get(ConsoleHelper.readString());
         return new ZipFileManager(zipPath);
