38dfdaad947f3bf474f7344638ae3e11bc814ff7
pm1975 <pm1975@windowslive.com>
2021-02-27 07:27:28 +0100

    Archiver (part 5). Done myself.

---
+++ 3.JavaMultithreading/src/com/codegym/task/task31/task3110/command/ZipCommand.java
@@ -0,0 +1,8 @@
+package com.codegym.task.task31.task3110.command;
+
+public abstract class ZipCommand implements Command {
+    @Override
+    public void execute() throws Exception {
+
+    }
+}
---
+++ 3.JavaMultithreading/src/com/codegym/task/task31/task3110/command/ZipExtractCommand.java
@@ -0,0 +1,4 @@
+package com.codegym.task.task31.task3110.command;
+
+public class ZipExtractCommand extends ZipCommand{
+}
