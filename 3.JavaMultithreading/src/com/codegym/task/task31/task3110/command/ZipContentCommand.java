139111a62b1d5c19ef5bcc2c052a0f1b0762a047
pm1975 <pm1975@windowslive.com>
2021-03-04 20:48:46 +0100

    Archiver (part 14). All done myself.

--- 3.JavaMultithreading/src/com/codegym/task/task31/task3110/command/ZipContentCommand.java
+++ 3.JavaMultithreading/src/com/codegym/task/task31/task3110/command/ZipContentCommand.java
@@ -1,8 +1,21 @@
 package com.codegym.task.task31.task3110.command;
 
+import com.codegym.task.task31.task3110.ConsoleHelper;
+import com.codegym.task.task31.task3110.FileProperties;
+import com.codegym.task.task31.task3110.ZipFileManager;
+
+import java.util.List;
+
 public class ZipContentCommand extends ZipCommand {
     @Override
     public void execute() throws Exception {
-
+        ConsoleHelper.writeMessage("Viewing contents of the archive.");
+        ZipFileManager fileManager = getZipFileManager();
+        ConsoleHelper.writeMessage("Archive contents:");
+        List<FileProperties> fileList = fileManager.getFileList();
+        for (FileProperties fileProperties : fileList) {
+            ConsoleHelper.writeMessage(fileProperties.toString());
+        }
+        ConsoleHelper.writeMessage("Archive contents viewed.");
     }
 }
