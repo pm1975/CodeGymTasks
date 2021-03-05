d74f64e744096d152378f4ef09fba0a026edb039
pm1975 <pm1975@windowslive.com>
2021-03-05 20:41:38 +0100

    28.8 MEDIUM task2806. Introducing Executors (before).

---
+++ 3.JavaMultithreading/src/com/codegym/task/task28/task2806/Solution.java
@@ -0,0 +1,33 @@
+package com.codegym.task.task28.task2806;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
+/* 
+Introducing Executors
+
+*/
+
+public class Solution {
+    public static void main(String[] args) throws InterruptedException {
+        // Add your code here
+
+        /* Example output
+pool-1-thread-2, localId=2
+pool-1-thread-1, localId=1
+pool-1-thread-3, localId=3
+pool-1-thread-1, localId=7
+pool-1-thread-1, localId=9
+pool-1-thread-4, localId=4
+pool-1-thread-5, localId=5
+pool-1-thread-2, localId=6
+pool-1-thread-1, localId=10
+pool-1-thread-3, localId=8
+         */
+    }
+
+    private static void doExpensiveOperation(int localId) {
+        System.out.println(Thread.currentThread().getName() + ", localId="+localId);
+    }
+}
