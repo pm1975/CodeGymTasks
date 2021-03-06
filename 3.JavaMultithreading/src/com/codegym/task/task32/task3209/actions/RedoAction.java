bdb3a9c57733d45804231e20a502d6af7c0abebb
pm1975 <pm1975@windowslive.com>
2021-03-06 20:25:00 +0100

    HTML Editor (part 8). Completed.

---
+++ 3.JavaMultithreading/src/com/codegym/task/task32/task3209/actions/RedoAction.java
@@ -0,0 +1,18 @@
+package com.codegym.task.task32.task3209.actions;
+
+import com.codegym.task.task32.task3209.View;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+
+public class RedoAction extends AbstractAction {
+    private View view;
+
+    public RedoAction(View view) {
+        this.view = view;
+    }
+
+    public void actionPerformed(ActionEvent e) {
+
+    }
+}
