f889ac28a6e1f90d9c1f093bbdb38926ce2e2aa3
pm1975 <pm1975@windowslive.com>
2021-03-10 22:16:52 +0100

    29.9 Caching

--- 3.JavaMultithreading/src/com/codegym/task/task29/task2907/Solution.java
+++ 3.JavaMultithreading/src/com/codegym/task/task29/task2907/Solution.java
@@ -10,9 +10,11 @@ This weird BigDecimal
 public class Solution {
     public static void main(String[] args) {
         System.out.println(getValue(1.1d, 1.2d));
+        System.out.println(getValue(-1.1d, 1.1d));
+        System.out.println(getValue(.100d, 1.100d));
     }
 
     public static BigDecimal getValue(double v1, double v2) {
-        return new BigDecimal(v1).add(new BigDecimal(v2));
+        return new BigDecimal(String.valueOf(v1)).add(new BigDecimal(String.valueOf(v2)));
     }
 }
