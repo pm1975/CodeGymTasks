39a5cf55b541538f6157d576f2288bf68934595d
pm1975 <pm1975@windowslive.com>
2021-03-29 21:13:30 +0200

    Numeral system converter. Before.

---
+++ 3.JavaMultithreading/src/com/codegym/task/task30/task3001/Solution.java
@@ -0,0 +1,29 @@
+package com.codegym.task.task30.task3001;
+
+/* 
+Numeral system converter
+
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        Number number = new Number(NumberSystemType._10, "6");
+        Number result = convertNumberToOtherNumberSystem(number, NumberSystemType._2);
+        System.out.println(result);    // 110 expected
+
+        number = new Number(NumberSystemType._16, "6df");
+        result = convertNumberToOtherNumberSystem(number, NumberSystemType._8);
+        System.out.println(result);    // 3337 expected
+
+        number = new Number(NumberSystemType._16, "abcdefabcdef");
+        result = convertNumberToOtherNumberSystem(number, NumberSystemType._16);
+        System.out.println(result);    // abcdefabcdef expected
+    }
+
+    public static Number convertNumberToOtherNumberSystem(Number number, NumberSystem expectedNumberSystem) {
+        //write your code here
+        return null;
+    }
+
+
+}
