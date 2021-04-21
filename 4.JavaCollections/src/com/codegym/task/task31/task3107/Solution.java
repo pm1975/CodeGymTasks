12aaaad65327acf7233adf7c7578598e45d9fea4
pm1975 <pm1975@windowslive.com>
2021-04-21 22:05:28 +0200

    Null object pattern

---
+++ 4.JavaCollections/src/com/codegym/task/task31/task3107/Solution.java
@@ -0,0 +1,17 @@
+package com.codegym.task.task31.task3107;
+
+/* 
+Null object pattern
+
+*/
+
+public class Solution {
+    private FileData fileData;
+
+    public Solution(String pathToFile) {
+    }
+
+    public FileData getFileData() {
+        return fileData;
+    }
+}
