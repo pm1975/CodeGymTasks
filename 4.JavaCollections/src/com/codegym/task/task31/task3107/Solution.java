f9e9f0195a0127fffeff874184ab9d9c5f17b325
pm1975 <pm1975@windowslive.com>
2021-04-21 22:19:38 +0200

    Path 101

--- 4.JavaCollections/src/com/codegym/task/task31/task3107/Solution.java
+++ 4.JavaCollections/src/com/codegym/task/task31/task3107/Solution.java
@@ -5,10 +5,20 @@ Null object pattern
 
 */
 
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 public class Solution {
     private FileData fileData;
 
     public Solution(String pathToFile) {
+        try {
+            Path path = Paths.get(pathToFile);
+            fileData = new ConcreteFileData(Files.isHidden(path), Files.isExecutable(path), Files.isDirectory(path), Files.isWritable(path));
+        } catch (Exception e) {
+            fileData = new NullFileData(e);
+        }
     }
 
     public FileData getFileData() {
