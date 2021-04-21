221fc0108f0a1bfcc2acd287f76695016ca5355d
pm1975 <pm1975@windowslive.com>
2021-04-21 21:32:02 +0200

    What's in the folder?

---
+++ 4.JavaCollections/src/com/codegym/task/task31/task3113/Solution.java
@@ -0,0 +1,14 @@
+package com.codegym.task.task31.task3113;
+
+import java.io.IOException;
+
+/* 
+What's in the folder?
+
+*/
+
+public class Solution {
+
+    public static void main(String[] args) throws IOException {
+    }
+}
