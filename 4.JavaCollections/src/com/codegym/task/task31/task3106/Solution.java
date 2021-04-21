e494c47ec784d1af951473294872f7e498987d14
pm1975 <pm1975@windowslive.com>
2021-04-21 22:37:02 +0200

    9 task prescribe.

--- 4.JavaCollections/src/com/codegym/task/task31/task3106/Solution.java
+++ 4.JavaCollections/src/com/codegym/task/task31/task3106/Solution.java
@@ -5,7 +5,46 @@ Unzipping a file
 
 */
 
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Stream;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipInputStream;
+import java.util.zip.ZipOutputStream;
+
 public class Solution {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        File result = new File(args[0]);
+        if (!result.exists()) {
+            result.createNewFile();
+        }
+        List<FileInputStream> fileInputStreams = new ArrayList<>();
+
+        List<String> fileNames = new ArrayList<>();
+        fileNames.addAll(Arrays.asList(args).subList(1, args.length));
+        Collections.sort(fileNames);
+
+        for (String name : fileNames) {
+            fileInputStreams.add(new FileInputStream(name));
+        }
+
+        try (ZipInputStream is = new ZipInputStream(new SequenceInputStream(Collections.enumeration(fileInputStreams)))) {
+            while (true) {
+                ZipEntry entry = is.getNextEntry();
+                if (entry == null) break;
+
+                try (OutputStream os = new BufferedOutputStream(new FileOutputStream(result))) {
+                    final int bufferSize = 1024;
+                    byte[] buffer = new byte[bufferSize];
+                    for (int readBytes; (readBytes = is.read(buffer, 0, bufferSize)) > -1; ) {
+                        os.write(buffer, 0, readBytes);
+                    }
+                    os.flush();
+                }
+            }
+        }
     }
 }
