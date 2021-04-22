5b10c735a7fb5c627711e83256018cc8424917cb
pm1975 <pm1975@windowslive.com>
2021-04-22 21:28:47 +0200

    Build a tree (part 1)

---
+++ 4.JavaCollections/src/com/codegym/task/task20/task2028/CustomTree.java
@@ -0,0 +1,24 @@
+package com.codegym.task.task20.task2028;
+
+import java.io.Serializable;
+import java.util.AbstractList;
+import java.util.List;
+
+/* 
+Build a tree (part 1)
+
+*/
+
+public class CustomTree extends AbstractList<String> implements Cloneable, Serializable {
+
+
+    @Override
+    public String get(int index) {
+        return null;
+    }
+
+    @Override
+    public int size() {
+        return 0;
+    }
+}
