9c7c7ed48331b16f3b5c2d9c41e9a27431a6fd62
pm1975 <pm1975@windowslive.com>
2021-05-05 06:46:01 +0200

    Serializing a zoo

--- 4.JavaCollections/src/com/codegym/task/task33/task3312/Zoo.java
+++ 4.JavaCollections/src/com/codegym/task/task33/task3312/Zoo.java
@@ -10,6 +10,13 @@ import java.util.List;
 public class Zoo {
     public List<Animal> animals = new ArrayList<>();
 
+    @JsonTypeInfo(use = JsonTypeInfo.Id.NAME,
+            include = JsonTypeInfo.As.PROPERTY,
+            property = "type")
+    @JsonSubTypes({
+            @JsonSubTypes.Type(value = Dog.class, name = "dog"),
+            @JsonSubTypes.Type(value = Cat.class, name = "cat")
+    })
     public static class Animal {
         public Animal(String name) {
             this.name = name;
@@ -18,6 +25,7 @@ public class Zoo {
         public String name;
     }
 
+    @JsonTypeName("dog")
     public static class Dog extends Animal {
 
         public double barkVolume;
@@ -26,7 +34,8 @@ public class Zoo {
             super(name);
         }
     }
-    
+
+    @JsonTypeName("cat")
     public static class Cat extends Animal {
         boolean likesCream;
         public int lives;
