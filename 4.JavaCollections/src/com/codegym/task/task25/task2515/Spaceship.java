a1375f894302331802f98e6f6735f95274af1b8b
pm1975 <pm1975@windowslive.com>
2021-05-07 15:37:24 +0200

    Space (part 12)

--- 4.JavaCollections/src/com/codegym/task/task25/task2515/Spaceship.java
+++ 4.JavaCollections/src/com/codegym/task/task25/task2515/Spaceship.java
@@ -1,8 +1,17 @@
 package com.codegym.task.task25.task2515;
 
 public class Spaceship extends BaseObject {
+    private double dx = 0;
 
-    public Spaceship(double x, double y, double radius) {
-        super(x, y, radius);
+    public Spaceship(double x, double y) {
+        super(x, y, 3);
+    }
+
+    public void moveLeft() {
+        dx--;
+    }
+
+    public void moveRight() {
+        dx++;
     }
 }
