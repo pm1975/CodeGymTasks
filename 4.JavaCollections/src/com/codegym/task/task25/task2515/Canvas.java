911d189d488c104ab31c736650d094e71f48a41c
pm1975 <pm1975@windowslive.com>
2021-05-07 05:40:41 +0200

    Space (part 8)

--- 4.JavaCollections/src/com/codegym/task/task25/task2515/Canvas.java
+++ 4.JavaCollections/src/com/codegym/task/task25/task2515/Canvas.java
@@ -22,4 +22,22 @@ public class Canvas {
     public char[][] getMatrix() {
         return matrix;
     }
+
+    public void setPoint(double x, double y, char c) {
+        int x1 = (int)Math.round(x);
+        int y1 = (int)Math.round(y);
+        if (x1 < 0 || y1 < 0 || x1 >= matrix[0].length || y1 >= matrix.length)
+            return;
+        matrix[y1][x1] = c;
+    }
+
+    public void drawMatrix(double x, double y, int[][] matrix, char c) {
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[0].length; j++) {
+                if (matrix[i][j] != 0) {
+                    setPoint(x + j, y + i, c);
+                }
+            }
+        }
+    }
 }
