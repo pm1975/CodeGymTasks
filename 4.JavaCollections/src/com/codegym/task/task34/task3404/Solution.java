5fe41ba523ade1fa94fa3a264a676f3a64c10734
pm1975 <pm1975@windowslive.com>
2021-05-10 04:55:35 +0200

    Recursion for mathematical expressions. BEFORE.

---
+++ 4.JavaCollections/src/com/codegym/task/task34/task3404/Solution.java
@@ -0,0 +1,21 @@
+package com.codegym.task.task34.task3404;
+
+/* 
+Recursion for mathematical expressions
+
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        solution.recurse("sin(2*(-5+1.5*4)+28)", 0); // Expected output: 0.5 6
+    }
+
+    public void recurse(final String expression, int countOperation) {
+        // Implement
+    }
+
+    public Solution() {
+        // Don't delete
+    }
+}
