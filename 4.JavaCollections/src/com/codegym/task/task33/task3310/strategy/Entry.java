1c28a7a6c5ef4e2b7384719d7b2d7a84ddc34a87
pm1975 <pm1975@windowslive.com>
2021-05-19 23:03:25 +0200

    Shortener (8)

--- 4.JavaCollections/src/com/codegym/task/task33/task3310/strategy/Entry.java
+++ 4.JavaCollections/src/com/codegym/task/task33/task3310/strategy/Entry.java
@@ -1,18 +1,19 @@
 package com.codegym.task.task33.task3310.strategy;
 
+import java.io.Serializable;
 import java.util.Objects;
 
-public class Entry {
-     Long key;
-     String value;
-     Entry next;
-     int hash;
+public class Entry implements Serializable {
+    Long key;
+    String value;
+    Entry next;
+    int hash;
 
     public Entry(int hash, Long key, String value, Entry next) {
-        this.hash = hash;
-        this.key = key;
         this.value = value;
         this.next = next;
+        this.key = key;
+        this.hash = hash;
     }
 
     public final Long getKey() {
@@ -27,6 +28,7 @@ public class Entry {
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
+
         Entry entry = (Entry) o;
 
         if (key != null ? !key.equals(entry.key) : entry.key != null) return false;
@@ -34,12 +36,12 @@ public class Entry {
 
     }
 
-    public int hashCode() {
+    public final int hashCode() {
         return Objects.hashCode(getKey()) ^ Objects.hashCode(getValue());
+
     }
 
-    @Override
-    public String toString() {
+    public final String toString() {
         return getKey() + "=" + getValue();
     }
 }
--- 4.JavaCollections/src/com/codegym/task/task33/task3310/strategy/HashMapStorageStrategy.java
+++ 4.JavaCollections/src/com/codegym/task/task33/task3310/strategy/HashMapStorageStrategy.java
@@ -1,41 +1,32 @@
 package com.codegym.task.task33.task3310.strategy;
 
-import com.codegym.task.task33.task3310.strategy.StorageStrategy;
-
 import java.util.HashMap;
-import java.util.Map;
-import java.util.Objects;
 
 public class HashMapStorageStrategy implements StorageStrategy {
-    private HashMap<Long, String> data = new HashMap<>();
+    private  HashMap<Long, String> data = new HashMap<>();
 
-    @Override
     public boolean containsKey(Long key) {
         return data.containsKey(key);
     }
 
-    @Override
     public boolean containsValue(String value) {
-        return data.containsValue(value);
+        return  data.containsValue(value);
     }
 
-    @Override
     public void put(Long key, String value) {
         data.put(key, value);
     }
 
-    @Override
+    public String getValue(Long key) {
+        return data.get(key);
+    }
+
     public Long getKey(String value) {
-        for (Long key : data.keySet()) {
-            if (data.get(key) == value) {
+        for(Long key : data.keySet()) {
+            if (data.get(key).equals(value)) {
                 return key;
             }
         }
         return null;
-    }
-
-    @Override
-    public String getValue(Long key) {
-        return data.get(key);
     }
 }
