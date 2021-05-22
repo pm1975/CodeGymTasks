d319e9672f8d6908102f661c7126e184948cd82e
pm1975 <pm1975@windowslive.com>
2021-05-22 18:50:43 +0200

    Checked exceptions

---
+++ 4.JavaCollections/src/com/codegym/task/task38/task3802/VeryComplexClass.java
@@ -0,0 +1,17 @@
+package com.codegym.task.task38.task3802;
+
+/* 
+Checked exceptions
+
+*/
+
+public class VeryComplexClass {
+    public void veryComplexMethod() throws Exception {
+        //write your code here
+        this.clone();
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
