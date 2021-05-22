3f25a8f70711931dfb1e307fc3d4233162af6f10
pm1975 <pm1975@windowslive.com>
2021-05-22 19:02:34 +0200

    Exception factory

---
+++ 4.JavaCollections/src/com/codegym/task/task38/task3804/ABCD_Factory.java
@@ -0,0 +1,23 @@
+package com.codegym.task.task38.task3804;
+
+import java.util.Locale;
+
+public class ABCD_Factory {
+    static Throwable getException(Enum enumm) {
+        if (enumm == null) return new IllegalArgumentException();
+        String message = enumm.name().toLowerCase().replaceAll("[_]", " ");
+        String first = message.substring(0, 1).toUpperCase();
+        message = first + message.substring(1);
+
+        switch (enumm.getClass().getSimpleName()) {
+            case "ApplicationExceptionMessage":
+                return new Exception(message);
+            case "DatabaseExceptionMessage":
+                return new RuntimeException(message);
+            case "UserExceptionMessage":
+                return new Error(message);
+            default:
+                return new IllegalArgumentException();
+        }
+    }
+}
